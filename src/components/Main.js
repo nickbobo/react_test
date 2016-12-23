7fccf104f86b5ebfea6dcd227bf24aa9d3d10c07
nickbobo <934858124@qq.com>
2016-12-23 18:02:55 +0800

    aa

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,58 +3,55 @@ require('styles/App.styl');
 
 import React from 'react';
 
-//let yeomanImage = require('../images/yeoman.png');
 
 class AppComponent extends React.Component {
-  handleClick(e){
-    if (this.props.arrange.isCenter) {
-      this.props.inverse();
-    } else {
-      this.props.center();
-    }
+  handleClick(index, items, e) {
+
+    console.info(index,items)
+    var stageDOM = items.refs.react_name,
+        stageW = stageDOM.scrollWidth,
+        stageH = stageDOM.scrollHeight,
+        halfStageW = Math.ceil(stageW / 2),
+        halfStageH = Math.ceil(stageH / 2);
+    console.info('---->>>',stageW,stageH,halfStageW,halfStageH);
     e.stopPropagation();
     e.preventDefault();
   }
 
-
+  //组件加载后
   componentDidMount() {
-    var stageDOM = this.refs.stage,
-      stageW = stageDOM.scrollWidth,
-      stageH = stageDOM.scrollHeight,
-      halfStageW = Math.ceil(stageW / 2),
-      halfStageH = Math.ceil(stageH / 2);
-      console.info("---->>>",stageH,halfStageW,halfStageH)
-
+    console.info(this.refs.stage);
   }
 
 
   render() {
-    var imageDatas = [{
-      name:'nick'
-    },{
-      name: '张三'
-    }]
-    var  // controllerUnits = [],
-    imgFigures = [];
-    imageDatas.forEach(function(value,index){
-        // console.info(value.name,index)
+      var imageDatas = [{
+        name: 'nick'
+      }, {
+        name: '张三'
+      }]
+      var imgFigures = [];
+      imageDatas.forEach(function(value,index){
+        var top = 0;
+        if(index!=0){
+          top=10
+        }
         imgFigures.push(
-            <div key  = {index} 
-            className = "react_name" 
-            onClick = {this.handleClick.bind(this)}
-              // onClick = {this.handleClick.bind(this)}
-              >
+         <div key = {index}
+            className = "react_name"
+            ref="react_name"
+            style = {{marginTop:top}}
+            onClick = {this.handleClick.bind('',index,this)}>
               {value.name},{index}
-            </div>
-          )
-    })
+          </div>
+        )
+      }.bind(this));
 
-    return (
-      <div className = "stage" ref = "stage">
-        { /*<img src={yeomanImage} alt="Yeoman Generator" />*/ }
-        <section className="notice" >{imgFigures}</section>
-      </div>
-    );
+      return(
+        <div className = "stage" ref="stage">
+          <section className = "notice">{imgFigures}</section>
+        </div>
+      )
   }
 }
 
